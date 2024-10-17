80328cd58e1505dfe388321c80e768548e9cd770
Lucía <lucigar0311@gmail.com>
2024-10-17 10:02:51 -0600

    Shopping cart

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\productController;
+use App\Http\Controllers\Api\shoppingcartController;
 use App\Models\shoppingcart;
 
 Route::get('/products', [productController::class, 'index']);
@@ -16,3 +17,11 @@ Route::delete('/products/{id}', [productController::class, 'destroy']); // Elimi
 Route::put('/products/{id}', [productController::class, 'update']);
 
 //Route::post('/products', [productController::class, 'checkout']); // Finalizar compra y generar factura
+
+Route::post('/cart', [shoppingcartController::class, 'store']); // Agregar producto al carrito
+
+Route::get('/cart', [shoppingcartController::class, 'index']);
+
+Route::delete('/cart/{product_id}', [shoppingcartController::class, 'destroy']); // Eliminar producto del carrito
+
+Route::post('/checkout', [shoppingcartController::class, 'checkout']); // Finalizar compra y generar factura
