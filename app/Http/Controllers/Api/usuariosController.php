2590712f616173022b4354b82535dceb9165bba2
Veronica Loaiza <veronicaloaiza040 gmail.com>
2024-10-22 14:34:44 -0600

    postman

--- app/Http/Controllers/Api/usuariosController.php
+++ app/Http/Controllers/Api/usuariosController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Usuarios;
+use Illuminate\Support\Facades\Validator;
 
 class usuariosController extends Controller
 {
