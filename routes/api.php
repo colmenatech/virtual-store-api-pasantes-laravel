1a9c92fb2b8a5f2211c2ed05678ff32fb0093456
Danna <dannapq06@gmail.com>
2024-10-23 09:59:27 -0600

    Implementando CRUD Categorias

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\productController;
 use App\Http\Controllers\Api\shoppingcartController;
 use App\Models\shoppingcart;
 use App\Http\Controllers\Api\usuariosController;
+use App\Http\Controllers\Api\categoriesController;
+
 
 //Productos
 
@@ -13,9 +15,9 @@ use App\Http\Controllers\Api\usuariosController;
 
 Route::get('/products', [productController::class, 'index']); //muestra todos los productos
 
-Route::post('/products', [productController::class, 'store']); // Agregar o crea producto 
+Route::post('/products', [productController::class, 'store']); // Agregar o crea producto
 
-Route::delete('/products/{id}', [productController::class, 'destroy']); // Eliminar producto 
+Route::delete('/products/{id}', [productController::class, 'destroy']); // Eliminar producto
 
 Route::put('/products/{id}', [productController::class, 'update']); //actualiza el producto
 
@@ -26,7 +28,7 @@ Route::put('/products/{id}', [productController::class, 'update']); //actualiza 
  Route::get('/products/{id}', [productController::class, 'show']); //muestra un producto ne específico o el detalle de este
 
 
- 
+
 //Usuarios
   Route::get('/Usuarios', [usuariosController::class, 'mostrar']);
 
@@ -35,3 +37,22 @@ Route::put('/products/{id}', [productController::class, 'update']); //actualiza 
 Route::post('/Usuarios', [usuariosController::class, 'registrar']);
 
 
+//CATEGORIAS
+//Route::middleware(['role:admin'])->group(function () {
+    // Rutas CRUD para categorías
+
+    // Ruta para crear una nueva categoría
+    Route::post('/categories', [CategoryController::class, 'store']);
+
+    // Ruta para actualizar una categoría existente por su ID
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+
+    // Ruta para eliminar una categoría por su ID
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+    // Ruta para listar todas las categorías
+    Route::get('/categories', [CategoryController::class, 'index']);
+
+    // Ruta para obtener los detalles de una categoría específica por su ID
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+//});
